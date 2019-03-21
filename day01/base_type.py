f95a7875357205ecb6745c8f3582643f844f2fc7
闫松林 <songlinlin1994@163.com>
2019-03-21 17:40:24 +0800

    新增list 方法

--- day01/base_type.py
+++ day01/base_type.py
@@ -41,19 +41,26 @@ def str_demo():
     # 打印 astr 的 类型; type(astr): 获取astr的类型
     print(type(astr))
 
+# 演示字符串拼接 : +
 def str_demo1():
     a= 'hello'
     b= ' world'
     return a+b
 
+# 字符串拼接: %s
 def str_demo2():
     a= 'hello '
     b= 250
-    print('a=%s b=%s'%(a,b))
-    s = str(b)
-    print(s)
-    print(type(s))
-    print(a+s)
+    # print(a+ str(b))
+    print('a 是 : %s;b 是 : %s'%(a,b))
+
+# 去掉两头空格
+def str_demo3():
+    astr = ' ysl '
+    # astr 是变量 也叫 对象  ,对象 . 可以调用对象的方法
+    print(astr)
+    print(astr.strip())
+
 
 # 12
 # 声明一个add 方法 参数有两个 aint,bint
@@ -74,17 +81,18 @@ def float_demo():
     # 打印 afloat 的 类型; type(afloat): 获取afloat的类型
     print(type(afloat))
 
-
-
-
 if __name__ == '__main__':
-    str_demo2()
+    str_demo3()
+    # str_demo2()
     # print(str_demo1())
     # float_demo()
     # str_demo()
     # int_demo()
     # 提取变量  ctrl+alt+V
     # 调用add方法 传入1, 2,得到返回值 ,赋值给result变量
-    # result = add(1, 2)
-    # print('1111')
+    # 指定参数传参
+    # result = add(bint=2,aint=1)
+    # print(result)
+    # 默认参数传参
+    # add(2,1)
     pass
