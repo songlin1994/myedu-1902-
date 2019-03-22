5939e40023e450826c3daa0873599e753b11a527
闫松林 <songlinlin1994@163.com>
2019-03-22 17:30:25 +0800

    第二天

---
+++ day02/yunsuanfu.py
@@ -0,0 +1,30 @@
+# 取余
+
+def jisuan(a,b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a % b)
+
+def duibi(a,b,c):
+    print(a > b)
+    print(a < b)
+    print(a == b)
+    print(a == c)
+    print(a >= b)
+    print(a != b)
+
+def deng(a):
+    a += 1  # a = a+1
+    print(a)
+    a *= 2  # a = a*2
+    print(a)
+    a -= 5  # a = a-5
+    print(a)
+    a /= 2  # a = a/2
+    print(a)
+
+if __name__ == '__main__':
+    a = 10
+
